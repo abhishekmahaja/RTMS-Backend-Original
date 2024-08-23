d5d76df402b5d8296a27dcb3bbdf0606b8228d43
abhishekmahaja <abhishekmahajan8285@gmail.com>
2024-08-23 13:30:27 +0530

    route

--- src/Middleware/protectedRoutes.js
+++ src/Middleware/protectedRoutes.js
@@ -1,42 +1,61 @@
 import jwt from "jsonwebtoken";
 import User from "../Models/userModel.js";
 
+// Adjust the import according to your project structure
+
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
-    if (!token) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
       return res
         .status(401)
         .json({ error: "Unauthorized - No Token Provided" });
     }
+
+    const token = authHeader.split(' ')[1];
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (!decoded) {
-      return res.status(401).json({ error: "Unauthorized - Invalid Token" });
-    }
-    const user = await User.findById(decoded._id).select("-password");
+
+
+    const user = await User.findById(decoded._id).select('-password');
+
+
+
     if (!user) {
       return res.status(404).json({ error: "User Not Found" });
     }
+
     req.user = user;
     next();
   } catch (error) {
     console.error("Error in protectRoute middleware:", error.message);
+
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ error: "Unauthorized - Token Expired" });
+    }
+    
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(401).json({ error: "Unauthorized - Invalid Token" });
+    }
+
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
 
 export const isManager = (req, res, next) => {
   try {
     const user = req.user;
 
-    if (user.roleInRTMS !== "manager" || user.roleInRTMS !== "owner") {
-      return res.json({
-        success: false,
-        message: "You are not authorised!",
-      });
+    if (user.roleInRTMS === "manager" || user.roleInRTMS === "owner") {
+      return  next()
     }
 
-    next();
+    return res.json({
+      success: false,
+      message: "You are not authorised"
+    })
   } catch (error) {
     console.error("Error in protectRoute middleware:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
