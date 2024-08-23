4818a4398f6529b8bc0c14b55395beed9af0bb8a
abhishekmahaja <abhishekmahajan8285@gmail.com>
2024-08-23 12:25:47 +0530

    status page

--- src/Middleware/protectedRoutes.js
+++ src/Middleware/protectedRoutes.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../Models/userModels.js";
+import User from "../Models/userModel.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
