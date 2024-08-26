ebc581a23ebea58d593429780f62e1aae5f762fd
abhishekmahaja <abhishekmahajan8285@gmail.com>
2024-08-26 12:27:31 +0530

    TO CHECK twuio

--- src/Helpers/helper.js
+++ src/Helpers/helper.js
@@ -19,6 +19,7 @@ const twilioClient = twilio(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
 );
+////
 
 // Send OTP Verification code
 export const sendOTPVerification = async ({
