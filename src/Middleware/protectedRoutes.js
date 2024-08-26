8480ce8540c593910d97ab1a5ff46b76696aaf4a
abhishekmahaja <abhishekmahajan8285@gmail.com>
2024-08-26 11:55:15 +0530

    vercel deploy

--- src/Middleware/protectedRoutes.js
+++ src/Middleware/protectedRoutes.js
@@ -7,20 +7,17 @@ export const protectRoute = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res
         .status(401)
         .json({ error: "Unauthorized - No Token Provided" });
     }
 
-    const token = authHeader.split(' ')[1];
+    const token = authHeader.split(" ")[1];
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-
-    const user = await User.findById(decoded._id).select('-password');
-
-
+    const user = await User.findById(decoded._id).select("-password");
 
     if (!user) {
       return res.status(404).json({ error: "User Not Found" });
@@ -31,11 +28,11 @@ export const protectRoute = async (req, res, next) => {
   } catch (error) {
     console.error("Error in protectRoute middleware:", error.message);
 
-    if (error.name === 'TokenExpiredError') {
+    if (error.name === "TokenExpiredError") {
       return res.status(401).json({ error: "Unauthorized - Token Expired" });
     }
-    
-    if (error.name === 'JsonWebTokenError') {
+
+    if (error.name === "JsonWebTokenError") {
       return res.status(401).json({ error: "Unauthorized - Invalid Token" });
     }
 
@@ -43,19 +40,18 @@ export const protectRoute = async (req, res, next) => {
   }
 };
 
-
 export const isManager = (req, res, next) => {
   try {
     const user = req.user;
 
     if (user.roleInRTMS === "manager" || user.roleInRTMS === "owner") {
-      return  next()
+      return next();
     }
 
     return res.json({
       success: false,
-      message: "You are not authorised"
-    })
+      message: "You are not authorised",
+    });
   } catch (error) {
     console.error("Error in protectRoute middleware:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
