dfe36a2f7f9e669b2500f1ce46baefad23049edb
abhishekmahaja <abhishekmahajan8285@gmail.com>
2024-08-30 23:32:16 +0530

    microcontroller

--- src/Models/externalDevicesModel.js
+++ src/Models/externalDevicesModel.js
@@ -21,19 +21,3 @@ const externalDevice = mongoose.model("externalDevice", externalDeviceSchema);
 
 export default externalDevice;
 
-// const externalDeviceSchema = new mongoose.Schema({
-//   data: {
-//     GIP: Number,
-//     CHP: Number,
-//     THP: Number,
-//     Battery_Percentage: Number,
-//     Solar_Power: Number,
-//     Communication: Number,
-//   },
-//   createAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-// const externalDevice = mongoose.model("externalDevice", externalDeviceSchema);
-// export default externalDevice;
