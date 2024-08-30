dea3e64f877e7f5a451ce0f8b8580d9bb52633b9
abhishekmahaja <abhishekmahajan8285@gmail.com>
2024-08-30 14:48:49 +0530

    microcontroller

--- src/Models/externalDevicesModel.js
+++ src/Models/externalDevicesModel.js
@@ -1,36 +1,39 @@
 import mongoose from "mongoose";
 
-// const externalDeviceSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//   },
-//   data: {
-//     type: String,
-//   },
-//   createAt: {
-//     type: Date,
-//     default: Date.now(),
-//   },
-// });
-
-// const externalDevice = mongoose.model("externalDevice", externalDeviceSchema);
-
-// export default externalDevice;
-
-
 const externalDeviceSchema = new mongoose.Schema({
+  // name: {
+  //   type: String,
+  // },
+  // data: {
+  //   type: String,
+  // },
   data: {
-    GIP: Number,
-    CHP: Number,
-    THP: Number,
-    Battery_Percentage: Number,
-    Solar_Power: Number,
-    Communication: Number,
+    type: Map, // Using Map allows storing key-value pairs
+    of: Number, // Values in the map are expected to be Numbers
   },
   createAt: {
     type: Date,
-    default: Date.now,
+    default: Date.now(),
   },
 });
+
 const externalDevice = mongoose.model("externalDevice", externalDeviceSchema);
+
 export default externalDevice;
+
+// const externalDeviceSchema = new mongoose.Schema({
+//   data: {
+//     GIP: Number,
+//     CHP: Number,
+//     THP: Number,
+//     Battery_Percentage: Number,
+//     Solar_Power: Number,
+//     Communication: Number,
+//   },
+//   createAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+// const externalDevice = mongoose.model("externalDevice", externalDeviceSchema);
+// export default externalDevice;
