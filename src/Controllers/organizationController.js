e2a0cc65ec155278988f1dbc25a73121130a3574
abhishekmahajan <abhishekmahajan8285@gmail.com>
2024-09-13 12:36:06 +0530

    organization,get,post,put

--- src/Controllers/organizationController.js
+++ src/Controllers/organizationController.js
@@ -21,10 +21,56 @@ export const organizationAddData = async (req, res) => {
 export const organizationUpdateData = async (req, res) => {
   try {
     const { id } = req.params;
-    const organizationUpdate = await Organization.findOneAndUpdate(id ,req.body, {
-      new: true,
-      runValidators: true,
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Enter The Id of Organization",
+      });
+    }
+
+    const organizationUpdate = await Organization.findOneAndUpdate(
+      {_id: id},
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!organizationUpdate) {
+      return res.status(404).json({
+        success: false,
+        message: "Organization Not Found",
+      });
+    }
+
+    res.json({
+      success: true,
+      message: "Data Update Successfully",
+      data: organizationUpdate,
     });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "Error updating organization data",
+    });
+  }
+};
+
+// Organization Get Data API
+export const organizationGetOneData = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const organizationUpdate = await Organization.findById(
+      {_id: id},
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
 
     if (!organizationUpdate) {
       return res.status(404).json({
@@ -45,4 +91,3 @@ export const organizationUpdateData = async (req, res) => {
     });
   }
 };
-
--- src/Routes/organizationsRoutes.js
+++ src/Routes/organizationsRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   organizationAddData,
+  organizationGetOneData,
   organizationUpdateData,
 } from "../Controllers/organizationController.js";
 
@@ -10,7 +11,8 @@ const organizationRouter = express.Router();
 // Route to add Organization
 organizationRouter.post("/organization-add-data", organizationAddData);
 // Route to Update organization
-organizationRouter.put("/organization-update-data", organizationUpdateData);
+organizationRouter.put("/organization-update-data/:id", organizationUpdateData);
 // Route to Get Organization
+organizationRouter.get("/organization-get-one-data/:id", organizationGetOneData);
 
 export default organizationRouter;
