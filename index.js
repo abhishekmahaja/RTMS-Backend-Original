9ab6ef7d571afb9c40eb040beae11f7b5f267272
abhishekmahajan <abhishekmahajan8285@gmail.com>
2024-11-03 22:17:37 +0530

    change port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import organizationRouter from "./src/Routes/organizationsRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 connectCloudinary();
 
 app.use(
